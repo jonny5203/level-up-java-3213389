1a0a3a9867fbef1875c35ffa4578e55ce6cc7029
jonny5203 <jonnyanhvu@gmail.com>
2024-04-01 02:40:24 +0200

    finished problem 3

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,10 +1,23 @@
 package com.linkedin.javacodechallenges;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class App {
 
+    // password must have at least 6 char
+    // at least one uppercase
+    // at least one lower
+    // one number
     public static boolean isPasswordComplex(String password) {
+        // A regex to look for the pattern inside
+        // the password string
+        String regex = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)";
+        Matcher matcher = Pattern.compile(regex).matcher(password);
+        if (matcher.find() && password.length() >= 6) {
+            return true;
+        }
         return false;
     }
 
