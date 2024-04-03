7e6be70630eb1c85f7a9c4809b5327f838aedd9a
jonny5203 <jonnyanhvu@gmail.com>
2024-04-03 13:45:25 +0200

    finished challenge 1_3

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,10 +4,12 @@ package com.linkedin.javacodechallenges;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    // This is a game that will randomly give
+    // 10 points and you can decide for yourself if
+    // you want to leave with the 10 point or continue playing
+    // If continuing you can loose all the points
+    public static void main(String[] args) {
         DoubleOrNothing game = new DoubleOrNothing();
         game.playGame();
     }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,38 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
+  private int points = 10;
+
   public void playGame() {
-    // TODO: Implement method
+    // User start with 10
+    Random rand = new Random();
+    boolean gameIsOn = true;
+    Scanner input = new Scanner(System.in);
+    while(gameIsOn){
+      boolean result = rand.nextBoolean();
+      System.out.println("Enter 1 to play and 0 to quit");
+      int userChoice = input.nextInt();
+      if(userChoice == 1){
+        if (result) {
+          points *= 2;
+          System.out.println("You won, and your points now is: " + points);
+        } else {
+          points = 0;
+          System.out.println("You lost, your current points is 0");
+          gameIsOn = false;
+        }
+      } else if(userChoice == 0){
+        System.out.println("Thank you for playing, your points is" + points);
+        System.out.println("Hope to see you again");
+        gameIsOn = false;
+      }else{
+        System.out.println("Invalid input, please reenter");
+      }
+    }
+
   }
 }
