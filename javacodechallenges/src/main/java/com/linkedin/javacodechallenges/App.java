c71753e415f4af5369e12368abdc41648db01be2
jonny5203 <jonnyanhvu@gmail.com>
2024-04-03 15:40:12 +0200

    2-1 finished

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,20 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    // Create function to calculate the date that's 
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class App {
+    // Create function to calculate the date that's
     // 100 days from now
 
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        LocalDate oneHundredDaysFromNow = calculateWhichDate(LocalDate.now(), 100);
+        System.out.println("100 days from now is... " +
+            oneHundredDaysFromNow);
+    }
+
+    private static LocalDate calculateWhichDate(LocalDate date, int daysInFuture) {
+        return date.plusDays(daysInFuture);
     }
 }
